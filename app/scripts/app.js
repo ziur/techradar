4fad5cbd17a771123fada75436e4707178c50e10
Alejandro Ruiz <aruiz@pros.com>
2014-02-18 19:44:36 -0400

    Create radar wizard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,12 +6,17 @@ var techRadarApp = angular.module('techradarApp', [
         'ngSanitize',
         'ngRoute',
         'ui.bootstrap',
+        'mgo-angular-wizard'
     ])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
                 templateUrl: 'views/main.html',
                 controller: 'MainCtrl'
+            })
+            .when('/new-radar', {
+                templateUrl: 'views/newRadar.html',
+                controller: 'VoteCtrl'
             })
             .when('/vote', {
                 templateUrl: 'views/vote.html',
