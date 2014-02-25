fd9f6148a5f5b0b2127d5b6d8f922d50056e6499
Alejandro Ruiz <aruiz@pros.com>
2014-02-25 10:05:59 -0400

    first working version of login

--- app/scripts/directives/loginPopUp.js
+++ app/scripts/directives/loginPopUp.js
@@ -1,24 +1,37 @@
-
+/*
 techRadarApp.run(function($rootScope) {
     $('.dropdown-menu').find('form').click(function (e) {
+        console.log('before click');
         e.stopPropagation();
+        console.log('after click');
     });
 });
-
+*/
 techRadarApp.directive('loginPopUp', ['loginService', function(loginService) {
     return {
         restrict: 'E',
         replace: true,
+        transclude: false,
         scope: {},
         controller: function($scope) {
             $scope.user = {};
 
             $scope.login = function() {
+                $('.dropdown-menu').hide();
                 console.log('Before loggin');
-                loginService.login({'userName':user.name, 'password':user.password})
+                loginService.login({'userName':$scope.user.name, 'password':$scope.user.password});
                 console.log('After loggin');
             };
         },
-        templateUrl: 'views/directives/loginPopUp.html'
+        templateUrl: 'views/directives/loginPopUp.html',
+        link: function(scope , element){
+            $('.dropdown-menu').find('form').click(function (e) {
+                e.stopPropagation();
+            });
+
+            /*element.bind("click", function(e){
+                e.stopPropagation();
+            });*/
+        }
     };
 }]);
